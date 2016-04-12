5af56b68390c4f7da58b140d9c29d07128a2bf2d
Michal Privoznik <mprivozn@redhat.com>
2016-04-12 11:38:12 +0200

    virNetClientStreamNew: Track origin stream
    This has no real added value right now,
    but is going to be very helpful later.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/remote/remote_driver.c
+++ src/remote/remote_driver.c
@@ -5935,7 +5935,8 @@ remoteDomainMigratePrepareTunnel3(virConnectPtr dconn,
     memset(&args, 0, sizeof(args));
     memset(&ret, 0, sizeof(ret));
 
-    if (!(netst = virNetClientStreamNew(priv->remoteProgram,
+    if (!(netst = virNetClientStreamNew(st,
+                                        priv->remoteProgram,
                                         REMOTE_PROC_DOMAIN_MIGRATE_PREPARE_TUNNEL3,
                                         priv->counter)))
         goto done;
@@ -6859,7 +6860,8 @@ remoteDomainMigratePrepareTunnel3Params(virConnectPtr dconn,
         goto cleanup;
     }
 
-    if (!(netst = virNetClientStreamNew(priv->remoteProgram,
+    if (!(netst = virNetClientStreamNew(st,
+                                        priv->remoteProgram,
                                         REMOTE_PROC_DOMAIN_MIGRATE_PREPARE_TUNNEL3_PARAMS,
                                         priv->counter)))
         goto cleanup;
