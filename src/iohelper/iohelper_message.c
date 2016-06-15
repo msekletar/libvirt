bf300921ba65f401b6cd488ff6c33e1a7dfff096
Michal Privoznik <mprivozn@redhat.com>
2016-06-15 12:52:42 +0200

    iohelper_message: Introduce basic buffer read/write
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/iohelper/iohelper_message.c
+++ src/iohelper/iohelper_message.c
@@ -21,4 +21,144 @@
 
 #include <config.h>
 
+#include "internal.h"
 #include "iohelper_message.h"
+#include "viralloc.h"
+#include "virerror.h"
+#include "virfile.h"
+#include "virnetmessage.h"
+
+#define VIR_FROM_THIS VIR_FROM_STORAGE
+
+struct _iohelperCtl {
+    int fd;             /* FD used for read/write */
+    virNetMessagePtr msg;
+};
+
+iohelperCtlPtr
+iohelperCtlNew(int fd)
+{
+    iohelperCtlPtr ret;
+
+    if (VIR_ALLOC(ret) < 0 ||
+        !(ret->msg = virNetMessageNew(false)))
+        goto error;
+
+    ret->fd = fd;
+
+    return ret;
+
+ error:
+    iohelperCtlFree(ret);
+    return NULL;
+}
+
+
+void
+iohelperCtlFree(iohelperCtlPtr ctl)
+{
+    if (!ctl)
+        return;
+
+    virNetMessageFree(ctl->msg);
+    VIR_FREE(ctl);
+}
+
+
+int
+iohelperCtlGetFD(iohelperCtlPtr ctl)
+{
+    if (!ctl)
+        return -1;
+    return ctl->fd;
+}
+
+
+static int
+readOne(iohelperCtlPtr ctl,
+        size_t len)
+{
+    ssize_t nread;
+    virNetMessagePtr msg = ctl->msg;
+
+    /* Couple of checks that should never happen. */
+    if (msg->bufferLength) {
+        /* The message is not yet fully processed. */
+        return 0;
+    }
+
+    virNetMessageClear(msg);
+    if (VIR_REALLOC_N(msg->buffer, len) < 0)
+        return -1;
+
+    nread = saferead(ctl->fd, msg->buffer, len);
+    if (nread < 0) {
+        virNetMessageClear(msg);
+        virReportSystemError(errno, "%s", _("Unable to read from stream"));
+        return -1;
+    }
+
+    msg->bufferLength = nread;
+    return 0;
+}
+
+
+ssize_t
+iohelperReadBuf(iohelperCtlPtr ctl,
+                char *buf,
+                size_t len)
+{
+    size_t want;
+    virNetMessagePtr msg = ctl->msg;
+
+    if (!msg->bufferLength &&
+        readOne(ctl, len) < 0)
+        return -1;
+
+    want = msg->bufferLength - msg->bufferOffset;
+    if (want > len)
+        want = len;
+
+    memcpy(buf,
+           msg->buffer + msg->bufferOffset,
+           want);
+    msg->bufferOffset += want;
+
+    if (msg->bufferOffset == msg->bufferLength)
+        virNetMessageClear(msg);
+
+    return want;
+}
+
+
+ssize_t
+iohelperWriteBuf(iohelperCtlPtr ctl,
+                 const char *buf,
+                 size_t len)
+{
+    ssize_t nwritten, want;
+    virNetMessagePtr msg = ctl->msg;
+
+    if (len) {
+        if (VIR_REALLOC_N(msg->buffer,
+                          msg->bufferLength + len) < 0)
+            return -1;
+
+        msg->bufferLength += len;
+        memcpy(msg->buffer + msg->bufferOffset,
+               buf,
+               len);
+    }
+
+    want = msg->bufferLength - msg->bufferOffset;
+    nwritten = safewrite(ctl->fd,
+                         msg->buffer + msg->bufferOffset,
+                         want);
+    if (nwritten < 0) {
+        virReportSystemError(errno, "%s", _("Unable to write to stream"));
+        return -1;
+    }
+
+    msg->bufferOffset += nwritten;
+    return nwritten;
+}
--- src/iohelper/iohelper_message.h
+++ src/iohelper/iohelper_message.h
@@ -23,4 +23,21 @@
 #ifndef __VIR_IOHELPER_MESSAGE_H__
 # define __VIR_IOHELPER_MESSAGE_H__
 
+typedef struct _iohelperCtl iohelperCtl;
+typedef iohelperCtl *iohelperCtlPtr;
+
+iohelperCtlPtr iohelperCtlNew(int fd);
+
+void iohelperCtlFree(iohelperCtlPtr ctl);
+
+int iohelperCtlGetFD(iohelperCtlPtr ctl);
+
+ssize_t iohelperReadBuf(iohelperCtlPtr ctl,
+                        char *buf,
+                        size_t len);
+ssize_t
+iohelperWriteBuf(iohelperCtlPtr ctl,
+                 const char *buf,
+                 size_t len);
+
 #endif /* __VIR_IOHELPER_H__ */
