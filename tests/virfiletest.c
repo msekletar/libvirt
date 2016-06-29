5c981676bbf21bdd37682c2d060ff29966717d9a
Michal Privoznik <mprivozn@redhat.com>
2016-06-29 16:42:39 +0200

    virfiletest: Test virFileInData
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- tests/virfiletest.c
+++ tests/virfiletest.c
@@ -21,10 +21,22 @@
 #include <config.h>
 
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#ifdef __linux__
+# if HAVE_LINUX_MAGIC_H
+#  include <linux/magic.h>
+# endif
+# include <sys/statfs.h>
+#endif
 
 #include "testutils.h"
 #include "virfile.h"
 #include "virstring.h"
+
+#define VIR_FROM_THIS VIR_FROM_NONE
 
 
 #if defined HAVE_MNTENT_H && defined HAVE_GETMNTENT_R
@@ -117,12 +129,217 @@ testFileSanitizePath(const void *opaque)
     return ret;
 }
 
+typedef struct {
+    bool startData;
+    unsigned long long *lengths;
+    const char *dir;
+    unsigned int fileno;
+} seekTestData;
+
+
+#ifndef EXT4_SUPER_MAGIC
+# define EXT4_SUPER_MAGIC 0xef53
+#endif
+#ifndef XFS_SUPER_MAGIC
+# define XFS_SUPER_MAGIC 0x58465342
+#endif
+
+static int
+createSparseFile(int *retFD,
+                 const char *filename,
+                 unsigned int *blockSize,
+                 bool startData,
+                 unsigned long long *lengths)
+{
+    int ret = -1;
+    int fd;
+    bool inData = startData;
+    struct statfs fs;
+    size_t i;
+    unsigned long long lengthSum = 0;
+    char buf[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                  'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+                  'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+                  'y', 'z'};
+
+    if ((fd = open(filename, O_CREAT|O_RDWR, 0666)) < 0) {
+        fprintf(stderr, "Unable to create %s", filename);
+        goto cleanup;
+    }
+
+    if (fstatfs(fd, &fs) < 0) {
+        fprintf(stderr, "Unable to statfs %s", filename);
+        goto cleanup;
+    }
+
+    /* Now, only some filesystems are known to work well with
+     * SEEK_DATA and SEEK_HOLE. Proceed only with the tested
+     * ones. */
+    if (fs.f_type != EXT4_SUPER_MAGIC &&
+        fs.f_type != XFS_SUPER_MAGIC) {
+        *blockSize = 0;
+        return 0;
+    }
+
+    *blockSize = fs.f_bsize;
+
+    for (i = 0; lengths[i]; i++) {
+        unsigned long long len = *blockSize * lengths[i];
+
+        lengthSum += len;
+
+        if (inData) {
+            /* write @len bytes */
+            while (len) {
+                ssize_t nwritten;
+                size_t want = len;
+                if (want > sizeof(buf))
+                    want = sizeof(buf);
+
+                if ((nwritten = safewrite(fd, buf, want)) < 0) {
+                    fprintf(stderr, "\nUnable to write %s\n", filename);
+                    goto cleanup;
+                }
+                len -= nwritten;
+            }
+        } else {
+            if (lseek(fd, len, SEEK_CUR) == (off_t) -1) {
+                fprintf(stderr,
+                        "\nUnable to seek %llu bytes in %s\n", len, filename);
+                goto cleanup;
+            }
+        }
+
+        inData = !inData;
+    }
+
+    if (ftruncate(fd, lengthSum) < 0) {
+        fprintf(stderr, "\nUnable to truncate %s to %llu", filename, lengthSum);
+        goto cleanup;
+    }
+
+    /* Now seek to the beginning of the file. */
+    if (lseek(fd, 0, SEEK_SET) == (off_t) -1) {
+        fprintf(stderr,
+                "\nUnable to seek to the beginning of %s\n", filename);
+        goto cleanup;
+    }
+
+    *retFD = fd;
+    fd = -1;
+    ret = 0;
+ cleanup:
+    VIR_FORCE_CLOSE(fd);
+    return ret;
+}
+
+
+static int
+testFileInData(const void *opaque)
+{
+    const seekTestData *data = opaque;
+    char *filename = NULL;
+    unsigned int blockSize;
+    int ret = -1;
+    int fd = -1;
+    int realInData = data->startData, inData;
+    unsigned long long len;
+    size_t i;
+    const off_t seekOffset = 64;
+
+    if (virAsprintf(&filename, "%s/sparse-test-%u", data->dir, data->fileno) < 0)
+        goto cleanup;
+
+    if (createSparseFile(&fd, filename, &blockSize, data->startData, data->lengths) < 0)
+        goto cleanup;
+
+    if (!blockSize) {
+        /* Underlying filesystem doesn't support SEEK_DATA and
+         * SEEK_HOLE. Skip the test. */
+        ret = EXIT_AM_SKIP;
+        goto cleanup;
+    }
+
+    for (i = 0; data->lengths[i]; i++) {
+        unsigned long long realLen = blockSize * data->lengths[i];
+
+        while (realLen) {
+            /* Check what the function thinks. */
+            if (virFileInData(fd, &inData, &len) < 0) {
+                fprintf(stderr, "\nvirFileInData failed\n");
+                goto cleanup;
+            }
+
+            /* Now compare it with reality. */
+            if (inData != realInData) {
+                fprintf(stderr, "\nExpected inData = %d got = %d\n", realInData, inData);
+                goto cleanup;
+            }
+
+            if (len != realLen) {
+                fprintf(stderr, "\nExpected length = %lld got = %lld", realLen, len);
+                goto cleanup;
+            }
+
+            /* And seek to next position. */
+            if (lseek(fd, MIN(seekOffset, realLen), SEEK_CUR) == (off_t) -1) {
+                fprintf(stderr, "\nUnable to seek in %s\n", filename);
+                goto cleanup;
+            }
+
+            realLen -= MIN(seekOffset, realLen);
+        }
+
+        realInData = !realInData;
+    }
+
+    /* Here we are at the EOF. virFileInData should return:
+     * inData = 0, len = 0. Check for that. */
+    if (virFileInData(fd, &inData, &len) < 0) {
+        fprintf(stderr, "\nvirFileInData failed\n");
+        goto cleanup;
+    }
+
+    if (inData) {
+        fprintf(stderr, "\nExpected inData = 0 got = %d\n", inData);
+        goto cleanup;
+    }
+
+    if (len) {
+        fprintf(stderr, "\nExpected length = 0 got = %lld", len);
+        goto cleanup;
+    }
+
+    ret = 0;
+ cleanup:
+    if (filename && !getenv("LIBVIRT_SKIP_CLEANUP"))
+        unlink(filename);
+    VIR_FREE(filename);
+    VIR_FORCE_CLOSE(fd);
+    return ret;
+}
+
+
+#define TESTDIRTEMPLATE abs_builddir "/virfiletestdir-XXXXXX"
+
 
 static int
 mymain(void)
 {
     int ret = 0;
     struct testFileSanitizePathData data1;
+    char *testdir;
+    unsigned int fileno = 0;
+
+    if (VIR_STRDUP_QUIET(testdir, TESTDIRTEMPLATE) < 0) {
+        VIR_TEST_DEBUG("Out of memory\n");
+        abort();
+    }
+
+    if (!mkdtemp(testdir)) {
+        VIR_TEST_DEBUG("Cannot create fakerootdir");
+        abort();
+    }
 
 #if defined HAVE_MNTENT_H && defined HAVE_GETMNTENT_R
 # define MTAB_PATH1 abs_srcdir "/virfiledata/mounts1.txt"
@@ -185,6 +402,34 @@ mymain(void)
     DO_TEST_SANITIZE_PATH_SAME("gluster://bar.baz//////fooo/hoo");
     DO_TEST_SANITIZE_PATH_SAME("gluster://bar.baz/fooo//hoo");
     DO_TEST_SANITIZE_PATH_SAME("gluster://bar.baz/fooo///////hoo");
+
+    /* Test virFileInData.
+     * This test automatically creates the file and calls virFileInData over it.
+     * @dataS: whether the file should start with data section.
+     * @args: array of lengths of sections in blocks for each section.
+     *
+     * For instance: DO_SEEK_TEST(true, 1, 2, 3) will create test file with 1
+     * block of data followed by 2 blocks of hole followed by 3 blocks of data.
+     */
+#define DO_SEEK_TEST(dataS, ...)                                        \
+    do {                                                                \
+        unsigned long long lengths[] = {__VA_ARGS__, 0};                \
+        seekTestData data = {.dir = testdir, .fileno = ++fileno,        \
+            .startData = dataS, .lengths = lengths};                    \
+        if (virTestRun(virTestCounterNext(), testFileInData, &data) < 0)\
+            ret = -1;                                                   \
+    } while (0)
+
+    virTestCounterReset("virFileInData ");
+    DO_SEEK_TEST(true, 1, 2, 3);
+    DO_SEEK_TEST(true, 1, 1, 1);
+    DO_SEEK_TEST(false, 1, 2, 3);
+    DO_SEEK_TEST(false, 1, 1, 1);
+
+    if (getenv("LIBVIRT_SKIP_CLEANUP") == NULL)
+        virFileDeleteTree(testdir);
+
+    VIR_FREE(testdir);
 
     return ret != 0 ? EXIT_FAILURE : EXIT_SUCCESS;
 }
