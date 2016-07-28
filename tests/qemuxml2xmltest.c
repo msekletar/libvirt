71698d61986ce122fd73ec7e4e555714a0ffc2bf
Michal Privoznik <mprivozn@redhat.com>
2016-07-28 18:54:18 +0200

    Introduce NVDIMM memory model
    NVDIMM is new type of memory introduced into QEMU 2.6. The idea
    is that we have a Non-Volatile memory module that keeps the data
    persistent across domain reboots.
    
    At the domain XML level, we already have some representation of
    'dimm' modules. Long story short, we have <memory/> element that
    lives under <devices/>. Now, the element even has @model
    attribute which we can use to introduce new memory type:
    
        <memory model='nvdimm'>
          <source>
            <path>/tmp/nvdimm</path>
          </source>
          <target>
            <size unit='KiB'>523264</size>
            <node>0</node>
          </target>
        </memory>
    
    So far, this is just a XML parser/formatter extension. QEMU
    driver implementation is in the next commit.
    
    For more info on NVDIMM visit the following web page:
    
        http://pmem.io/
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- tests/qemuxml2xmltest.c
+++ tests/qemuxml2xmltest.c
@@ -871,6 +871,7 @@ mymain(void)
     DO_TEST("memory-hotplug", NONE);
     DO_TEST("memory-hotplug-nonuma", NONE);
     DO_TEST("memory-hotplug-dimm", NONE);
+    DO_TEST("memory-hotplug-nvdimm", NONE);
     DO_TEST("net-udp", NONE);
 
     DO_TEST("video-virtio-gpu-device", NONE);
